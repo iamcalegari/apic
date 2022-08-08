3284b21e2bd83a7656c6b369c9361041a060de8f
iamcalegari <iamcalegari20@gmail.com>
2022-08-07 23:51:41 -0300

    wip

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,9 +6,9 @@ const { coletarVetor } = require("../services/coletarVetores");
 const Post = require("../models/post.model");
 
 routes.get("/api/vetores/:id/baixar", async (req, res) => {
-  const { vetor } = await Post.findById(req.params.id);
+  // const { vetor } = await Post.findById(req.params.id);
 
-  console.log(vetor);
+  // console.log(vetor);
 
   // fs.writeFile(
   //   resolve(__dirname, "..", "..", "public", "vetor.json"),
