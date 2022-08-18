d9a0770590eeda77f31a8ffc6d476ccd432327b7
iamcalegari <iamcalegari20@gmail.com>
2022-08-18 16:25:12 -0300

    wip

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,57 +2,12 @@ const routes = require("express").Router();
 const fs = require("fs");
 const { resolve } = require("path");
 
-const { coletarVetor } = require("../services/coletarVetores");
 const Post = require("../models/post.model");
+import { split } from "../services/cortar-vetores";
 
 routes.get("/api/vetores/split/:m", async (req, res) => {
-  const coletarVetor = await Post.find();
-
-  const vetores = coletarVetor.map((vet) => vet.vetor);
-
-  let arr = [];
-
-  vetores.forEach((vetor) => {
-    arr = arr.concat(vetor);
-  });
-
-  const corte = (array, index) => {
-    const numeroVetores = Math.ceil(array.length / index);
-    let arrayCortado = Array(numeroVetores).fill(0);
-    let indexLimite = index;
-    let inicio = 0;
-
-    for (let i = 0; i < numeroVetores; i++) {
-      arrayCortado[i] = [];
-      for (let j = inicio; j < inicio + index; j++) {
-        if (j < array.length) {
-          arrayCortado[i].push(array[j]);
-        }
-      }
-      inicio = indexLimite;
-      indexLimite += index;
-    }
-    return arrayCortado;
-  };
-
-  const arrayCortado = corte(arr, +req.params.m);
-
-  try {
-    const response = await Post.find();
-    for (let i = 0; i < response.length; i++) {
-      const posts = await Post.findOneAndDelete();
-    }
-  } catch (err) {
-    console.log(err);
-  }
-
-  arrayCortado.map(async (vetor) => {
-    await Post.create({
-      vetor,
-    });
-  });
-
-  return res.json(arrayCortado);
+  const array = split(req.params.m);
+  return res.json(array);
 });
 
 // Rota para publicar um vetor
---
+++ src/services/cortar-vetores.js
@@ -0,0 +1,54 @@
+const Post = require("../models/post.model");
+
+const split = (m) => {
+  const coletarVetor = await Post.find();
+
+  const vetores = coletarVetor.map((vet) => vet.vetor);
+
+  let arr = [];
+
+  vetores.forEach((vetor) => {
+    arr = arr.concat(vetor);
+  });  
+
+  const arrayCortado = corte(arr, +m);
+
+  try {
+    const response = await Post.find();
+    for (let i = 0; i < response.length; i++) {
+      const posts = await Post.findOneAndDelete();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  arrayCortado.map(async (vetor) => {
+    await Post.create({
+      vetor,
+    });
+  });
+
+  return arrayCortado;
+}
+
+const corte = (array, index) => {
+  const numeroVetores = Math.ceil(array.length / index);
+  let arrayCortado = Array(numeroVetores).fill(0);
+  let indexLimite = index;
+  let inicio = 0;
+
+  for (let i = 0; i < numeroVetores; i++) {
+    arrayCortado[i] = [];
+    for (let j = inicio; j < inicio + index; j++) {
+      if (j < array.length) {
+        arrayCortado[i].push(array[j]);
+      }
+    }
+    inicio = indexLimite;
+    indexLimite += index;
+  }
+  return arrayCortado;
+};
+
+
+export default split;
